626a669b835c8ec0db60f80d1f25e89b48922c0a
Benson Huang <benson@Bensons-MacBookPro.local>
2025-02-28 12:20:53 +0800

    [NVim] Add setup scripts for Mac and Linux.

---
+++ nvim/nvim_linux.sh
@@ -0,0 +1,7 @@
+# install nerd font
+# https://www.nerdfonts.com/font-downloads
+
+sudo apt-get install neovim
+sudo apt-get install ripgrep
+sudo apt-get install fzf
+
---
+++ nvim/nvim_mac.sh
@@ -0,0 +1,5 @@
+# install nerd font
+# https://www.nerdfonts.com/font-downloads
+
+brew install fzf
+brew install ripgrep
