59c5406a8a7633253fd690b8dfc0f53588bfaa61
root <root@bensonhuang.>
2025-02-28 21:34:30 +0800

    [ZSH] Modify installation script and .zshrc.

--- install/linux/zsh.sh
+++ install/linux/zsh.sh
@@ -4,12 +4,14 @@ set -e
 # ref: https://www.kwchang0831.dev/dev-env/ubuntu/oh-my-zsh
 sudo apt-get update
 sudo apt-get install zsh -y
-# download fonts
-wget https://github.com/romkatv/dotfiles-public/raw/master/.local/share/fonts/NerdFonts/MesloLGS%20NF%20Regular.ttf &&
-wget https://github.com/romkatv/dotfiles-public/raw/master/.local/share/fonts/NerdFonts/MesloLGS%20NF%20Bold.ttf &&
-wget https://github.com/romkatv/dotfiles-public/raw/master/.local/share/fonts/NerdFonts/MesloLGS%20NF%20Italic.ttf &&
-wget https://github.com/romkatv/dotfiles-public/raw/master/.local/share/fonts/NerdFonts/MesloLGS%20NF%20Bold%20Italic.ttf
+
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
-sed -i 's#^ZSH_THEME=.*$#ZSH_THEME="powerlevel10k/powerlevel10k"#' ~/.zshrc
+
 zsh
+sed -i 's#^ZSH_THEME=.*$#ZSH_THEME="powerlevel10k/powerlevel10k"#' ~/.zshrc
+# download fonts
+wget https://github.com/romkatv/dotfiles-public/raw/master/ .local/share/fonts/NerdFonts/MesloLGS%20NF%20Regular.ttf &&
+wget https://github.com/romkatv/dotfiles-public/raw/master/ .local/share/fonts/NerdFonts/MesloLGS%20NF%20Bold.ttf &&
+wget https://github.com/romkatv/dotfiles-public/raw/master/ .local/share/fonts/NerdFonts/MesloLGS%20NF%20Italic.ttf &&
+wget https://github.com/romkatv/dotfiles-public/raw/master/ .local/share/fonts/NerdFonts/MesloLGS%20NF%20Bold%20Italic.ttf
--- zsh/.zshrc
+++
@@ -1,6 +0,0 @@
-# ===== Vim-like key binding =====
-set -o vi
-bindkey -v 
-bindkey -M vicmd 'k' history-search-backward
-bindkey -M vicmd 'j' history-search-forward
-bindkey -M viins 'jk' vi-cmd-mode
