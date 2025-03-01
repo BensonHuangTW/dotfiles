fef25a12199bdfb863cadccf0e62e1b44ff8476d
root <root@bensonhuang.>
2025-03-01 10:15:09 +0800

    Refine cpp setup scripts (set a trap to return to the original directory on exit).

--- install/linux/cpp.sh
+++ install/linux/cpp.sh
@@ -1,4 +1,8 @@
-set -e
+# Save the original directory
+ORIGINAL_DIR=$(pwd)
+
+# Set a trap to return to the original directory on exit
+trap "cd \"$ORIGINAL_DIR\"" EXIT
 
 # gcc/g++
 sudo apt-get update
@@ -10,9 +14,11 @@ sudo apt install -y cmake
 # GoogleTest
 sudo apt-get install -y libgtest-dev
 cd /usr/src/gtest 
-sudo mkdir build
+sudo mkdir -p build
 cd build
 sudo cmake ..
 sudo make
 sudo cp lib/*.a /usr/lib
 
+# Return to the original directory
+cd "$ORIGINAL_DIR"
