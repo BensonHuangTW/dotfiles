e9a9f9bfaf1e1aaeb7e7849aef875dac82b859f6
Pin-Yao Huang <pinyaohuang@google.com>
2025-03-16 12:15:47 +0000

    [NVim] Add new keybinding and functionalities in telescope.

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -8,10 +8,33 @@ return {
         build = "make"
       }
     },
-    config = function()
-      -- Setting up telescope fzf
-      -- https://github.com/nvim-telescope/telescope-fzf-native.nvim
-      require("telescope").setup {
+    config = function(_, opts)
+      local slow_scroll = function(prompt_bufnr, direction)
+        local previewer = require("telescope.actions.state").get_current_picker(prompt_bufnr).previewer
+        local status = require("telescope.state").get_status(prompt_bufnr)
+
+        -- Check if we actually have a previewer and a preview window
+        if type(previewer) ~= "table" or previewer.scroll_fn == nil or status.preview_win == nil then
+          return
+        end
+
+        previewer:scroll_fn(1 * direction)
+      end
+
+      local full_page_scroll = function(prompt_bufnr, direction)
+        local previewer = require("telescope.actions.state").get_current_picker(prompt_bufnr).previewer
+        local status = require("telescope.state").get_status(prompt_bufnr)
+
+        -- Check if we actually have a previewer and a preview window
+        if type(previewer) ~= "table" or previewer.scroll_fn == nil or status.preview_win == nil then
+          return
+        end
+
+        local speed = vim.api.nvim_win_get_height(status.preview_win)
+        previewer:scroll_fn(speed * direction)
+      end
+
+      local defaults = {
         extensions = {
           fzf = {
             fuzzy = true,                   -- false will only do exact matching
@@ -26,9 +49,46 @@ return {
             hidden = true,
             file_ignore_patterns = {"^.git/"}
           }
-        }
+        },
+        mappings = {
+          i = {
+            ["<C-n>"] = function(...)
+              return require("telescope.actions").cycle_history_next(...)
+            end,
+            ["<C-p>"] = function(...)
+              return require("telescope.actions").cycle_history_prev(...)
+            end,
+            ["<C-f>"] = function(prompt_bufnr)
+              full_page_scroll(prompt_bufnr, 1)
+            end,
+            ["<C-b>"] = function(prompt_bufnr)
+              full_page_scroll(prompt_bufnr, -1)
+            end,
+            ["<C-e>"] = function(prompt_bufnr)
+              slow_scroll(prompt_bufnr, 1)
+            end,
+            ["<C-y>"] = function(prompt_bufnr)
+              slow_scroll(prompt_bufnr, -1)
+            end,
+            ["<C-q>"] = function(...)
+              require("telescope.actions").smart_send_to_qflist(...)
+              require("telescope.actions").open_qflist(...)
+            end,
+          },
+        },
+        path_display = function(path_opts, path)
+          path = path:gsub("^" .. vim.env.HOME, "~")
+          if type(path_display) == "function" then
+            path = path_display(path_opts, path)
+          end
+          return path
+        end,
       }
 
+      local final_opts = vim.tbl_deep_extend("force", defaults, opts or {})
+
+      require("telescope").setup(final_opts)
+      
       require("telescope").load_extension("fzf")
       local builtin = require("telescope.builtin")
       vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
